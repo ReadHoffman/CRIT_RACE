b2e1224a455bc45cd6e777c8305fafa88399085d
Read Hoffman <Read.Hoffman@Gmail.com>
2020-09-28 11:26:23 -0500

    Backprop now works!  Albeit for simple training examples, but still!

--- backpropagate.py
+++ backpropagate.py
@@ -7,38 +7,50 @@ Created on Wed Sep 23 16:42:37 2020
 import math
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
-#xor
-training_dataset = [[[0,1],1], [ [0,0], 0], [[1,0],1],[[1,1],0] ]
+##xor
+#training_dataset = [[[0,1],1], [ [0,0], 0], [[1,0],1],[[1,1],0] ]
+
+
+training_dataset = [ [ [1,4,5],[.1,.05] ] ]
+testing_dataset = training_dataset.copy()
+
 inputs,target = training_dataset[0]
+
+LEARNING_RATE=.01
 
 class Node:
     def __init__(self,layer_id,node_id,network_part):
         self.layer_id = layer_id
         self.node_id = node_id
-        self.bias = None
+        self.bias = .5
+        self.dbias = None
         self.network_part = network_part
         self.value = random.random()
-        self.dactivation = None # squish value
-        self.target = None #expected value
-        self.error = None # 1/2 * (current value-expected value)**2
-        self.derror = None #derivative of node with respect to error function
-        self.batch_cost = []
+        self.output = None
+        self.error = None 
+        self.target = None 
+        self.derror = None 
+        self.doutput = None
+        self.batch_errors = []
         
-    def activation(self,x):
+    def activation(self):
         #activation
-        return 1 / (1 + math.exp(-x))
-    
-    def dactivation(self,x):
-        self.dactivation =  self.activation(x)*(1-self.activation(x))
+        self.output =  1 / (1 + math.exp(-self.value))
     
     def calculate_error(self):
-        self.error = ((self.value-self.target)**2)/2
+        self.error = ((self.output-self.target)**2)/2
         
-    def derror(self):
-        self.derror = self.value-self.target
+    def calculate_derror(self):
+        self.derror = self.output-self.target
+    
+    def dactivation(self):
+        self.doutput =  self.output*(1-self.output)    
         
-        
+    def learn(self):
+        self.value = self.bias-LEARNING_RATE*self.dbias    
+
         
     
         
@@ -51,8 +63,11 @@ class Weight:
         self.node_id_end = node_id_end
         self.network_part = 'weight'
         self.value = random.random()
-        self.dactivation = None
-        self.batch_cost = []
+        self.derror = None
+        self.batch_errors = [] #future enhancement batch backprop
+        
+    def learn(self):
+        self.value = self.value-LEARNING_RATE*self.derror
         
 ## a collection of nodes (but can also include weights despite it not being a hidden layer)
 #class Layer:
@@ -76,9 +91,8 @@ class Weight:
         
 class Network:
     # nodes_input_list should be in the format of [input eg 2, hidden eg 5, hidden eg 5, hidden, etc, output eg 2]
-    def __init__(self,nodes_input_list,learning_rate=.01):
+    def __init__(self,nodes_input_list):
         self.nodes_input_list = nodes_input_list
-        self.learning_rate = learning_rate
         self.network = None
         self.total_error = None
         self.total_error_history = []
@@ -117,6 +131,15 @@ class Network:
                 # search layer for node matching weight's node id start
                 prior_node = [node for node in layer if node.node_id==weight.node_id_start][0]
         return prior_node
+        
+    def next_node(self,weight):
+        desired_layers = ['hidden','output']
+        #search net for layer matching the weight layer end
+        for i, layer in enumerate(self.network):
+            if layer[0].network_part in desired_layers and layer[0].layer_id==weight.layer_id_end:
+                # search layer for node matching weight's node id end
+                next_node = [node for node in layer if node.node_id==weight.node_id_end][0]
+        return next_node
     
     def prior_weights(self,node):
         desired_layers = ['weight']
@@ -124,108 +147,130 @@ class Network:
         for i, layer in enumerate(self.network):
             if layer[0].network_part in desired_layers and layer[0].layer_id_end==node.layer_id:
                 # search layer for node matching weight's node id start
-                prior_weights = [weight.value for weight in layer if weight.node_id_end==node.node_id]
+                prior_weights = [weight for weight in layer if weight.node_id_end==node.node_id]
         return prior_weights
+    
+    def next_weights(self,node):
+        desired_layers = ['weight']
+        #search net for weight layer end matching the node layer
+        for i, layer in enumerate(self.network):
+            if layer[0].network_part in desired_layers and layer[0].layer_id_start==node.layer_id:
+                # search layer for node matching weight's node id start
+                next_weights = [weight for weight in layer if weight.node_id_start==node.node_id]
+        return next_weights
                 
     def feed_forward(self):
-        # calc for each hidden layer and final ouput, we need to calc activation(input*weight)
         desired_layers = ['hidden','output']
         for i, layer in enumerate(self.network):
             #check if layer type is correct for feedforward calcs
             if layer[0].network_part in desired_layers:
                 # get layer length so we can reshape output to match
                 layer_len = len(layer)
-                # get inputs from prior nodes and activate them
-                inputs = [node.activation(node.value) for node in self.network[i-2]]
+                # activate prior node layer
+                [node.activation() for node in self.network[i-2]]
+                # get the activated output, these will be inputs into calc
+                inputs = [node.output for node in self.network[i-2]]
                 inputs_len = len(inputs)
-                #get weights that connect current layer to prior layer
+                #get weights that connect current node layer to prior node layer
                 weights = [weight.value for weight in self.network[i-1]]
                 
-                #column of first matrix must equal row of second matrix
+                # rule: column of first matrix must equal row of second matrix
                 inputs = np.reshape(inputs,(inputs_len,1))
                 weights = np.reshape(weights,(layer_len,inputs_len))
                   
-                layer_output = np.dot(np.asarray(weights),np.asarray(inputs))
-                layer_output = [item for sublist in layer_output for item in sublist]
+                layer_values = np.dot(np.asarray(weights),np.asarray(inputs))
+                layer_values = [item for sublist in layer_values for item in sublist]
                 #set those layer values equal to dot product output
                 
-                for j, output_value in enumerate(layer_output):
-                    self.network[i][j].value = output_value
+                for j, value in enumerate(layer_values):
+                    self.network[i][j].value = value+self.network[i][j].bias
+                    
+        #activate output layer nodes to finalize output
+        for i, node in enumerate(self.network[-1]):
+            self.network[-1][i].activation()
+
                 
-    def train(self,training_dataset,iterations=10):
+    def train(self,training_dataset,iterations):
         
         iteration = 0
         while iteration<iterations:
             #pick a random training example
-            inputs,target = random.choice(training_dataset)
+            inputs,targets = random.choice(training_dataset)
             #x values input
             for i, node in enumerate(self.network[0]):
                 node.value = inputs[i]
-                
-            error_list = []
-            #y values output
+            
+            #assign target values to output layer nodes
             for i, node in enumerate(self.network[-1]):
-                self.network[-1][i].target = target
-                self.network[-1][i].calculate_error()
-                error_list.append(self.network[-1][i].error)
+                node.target = targets[i]
+            
+            net.feed_forward()
+            
+            error_list = []
+            network_len = len(self.network)
+            for i in range(network_len):
+                i = (i+1)*-1
+                if self.network[i][0].network_part == 'output':
+                    for node in self.network[i]:
+                        node.calculate_error() 
+                        error_list.append(node.error)
+                        node.calculate_derror()
+                        node.dactivation()
+                        node.dbias = node.derror*node.doutput
+                if self.network[i][0].network_part == 'weight':
+                    for weight in self.network[i]:
+                        weight.derror = self.next_node(weight).derror*self.next_node(weight).doutput*self.prior_node(weight).output
+                        weight.learn()
+                if self.network[i][0].network_part == 'hidden':
+                    for node in self.network[i]:
+                        node.dactivation()
+                        derrors = []
+                        for weight in self.next_weights(node):
+                            derror = self.next_node(weight).derror*self.next_node(weight).doutput*weight.value
+                            derrors.append(derror)
+                        node.derror = sum(derrors)
+                        node.dbias = node.derror*node.doutput
+                        node.learn()
             
             self.total_error = sum(error_list)
             #add error to history for graphing later
             self.total_error_history.append(self.total_error)
             
-            ### back propagation goes here ###
-            
-            # how much does each weight effect the total error
-            # d E^total/ d weight
-            
-            ## using chain rule
-            #start with last layer, node 0
-            #compute the derivative of error with respect to activation
-            # error is ((self.target-self.value)**2)/2
-            # derivative is value-trainy
-            
-            #compute derivative of activation with respect to Z  
-            ##### dactivation method should be available for every node
-            
-            #compute derivative of Z with respect to weight
-            # Z is h1*w1 + h2*w2 + hn*wn 
-            
-            network_len = len(self.network)
-            for i in range(network_len):
-                i = i*-1
-                if self.network[i][0].network_part in ['hidden','output']:
-                    self.network[i].derror()
-                    node.dactivation()
+#            for layer in self.network:
+#                if layer[0].network_part == 'weight':
+#                    for weight in layer:
+#                        weight.learn()
             
             ### end back propagation ###
             iteration+=1
         
-                
+    def test(self,testing_dataset):
+        test_ys = []
+        for records in testing_dataset:
+            #x values input
+            for i, node in enumerate(self.network[0]):
+                node.value = records[0][i]
+            self.feed_forward()
+            node_guesses = []
+            for node in self.network[-1]:
+                node.calculate_error()
+                node_guesses.append(node.output)
+            test_ys.append([records[0],records[1],node_guesses])
+            
+        return test_ys
         
-net = Network([2,2,1])
+net = Network([3,8,2])
 net.create_nodes()
-net.feed_forward()
-net.train(training_dataset,iterations=10)
-print(net.total_error)
+#net.feed_forward()
 
+
+net.train(training_dataset,iterations=10000)
+plt.plot(net.total_error_history)
+#print('start error: ',net.total_error_history[0],'/// end error: ',net.total_error_history[-1])
+print(net.test(testing_dataset))
 
 # example dot product
 #matrix_input_test1 = [[1],[2]]
 #matrix_weight_test1 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
 #np.dot(matrix_weight_test1,matrix_input_test1)
 
-
-def backpropagation(self,y, z_s, a_s):
-  dw = []  # dC/dW
-  db = []  # dC/dB
-  deltas = [None] * len(self.weights)  # delta = dC/dZ  known as error for each layer
-  # insert the last layer error
-  deltas[-1] = ((y-a_s[-1])*(self.getDerivitiveActivationFunction(self.activations[-1]))(z_s[-1]))
-  # Perform BackPropagation
-  for i in reversed(range(len(deltas)-1)):
-    deltas[i] = self.weights[i+1].T.dot(deltas[i+1])*(self.getDerivitiveActivationFunction(self.activations[i])(z_s[i]))        
-    batch_size = y.shape[1]
-    db = [d.dot(np.ones((batch_size,1)))/float(batch_size) for d in deltas]
-    dw = [d.dot(a_s[i].T)/float(batch_size) for i,d in enumerate(deltas)]
-    # return the derivitives respect to weight matrix and biases
-    return dw, db
